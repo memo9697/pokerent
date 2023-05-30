e7be16376d23977c3cf31571d6dc952c3664545a
Hugo Walandowitsch <hugowalando@gmail.com>
2023-05-30 13:25:34 +0200

    deleted column description in pokemon table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_101840) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_112026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_101840) do
     t.string "first_type"
     t.string "second_type"
     t.boolean "shiny_flag"
-    t.text "description"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
