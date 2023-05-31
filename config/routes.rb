8deda07966d12d676fe3df6833bc795fe56832ab
Hugo Walandowitsch <hugowalando@gmail.com>
2023-05-31 18:27:11 +0200

    ajout de la route dashboard , du controller et une view de base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :pokemons
+  get '/dashboard', to: 'dashboard#index', as: 'dashboard'
   # get "pokemons", to: "pokemons#index", as: :pokemons
   # get "pokemons/new", to: "pokemons#new", as: :new_pokemon
   # post "pokemons", to: "pokemons#create"
